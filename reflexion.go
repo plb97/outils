8118795eac2913baa2d8f831bfbc7d12909a8c89
plb97 <p.lhb97@gmail.com>
2017-06-11 17:39:05 +0200

    petits changements

--- reflexion.go
+++ reflexion.go
@@ -1,4 +1,8 @@
-// Copyright 2017 plb97. All rights reserved.
+// Copyright 2017 plb97.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE (French)
+// or LICENSE_EN (English) file.
 package outils
 
 import (
