972d727095a3fb17c6383b3636df118016538c61
plb97 <p.lhb97@gmail.com>
2017-06-11 17:47:29 +0200

    petits changements

--- reflexion.go
+++ reflexion.go
@@ -1,8 +1,8 @@
-// Copyright 2017 plb97.
+// Copyright (c) 2017 plb97.
 // All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE (French)
-// or LICENSE_EN (English) file.
+// Use of this source code is governed by a CeCILL-B_V1
+// (BSD-style) license that can be found in the
+// LICENSE (French) or LICENSE_EN (English) file.
 package outils
 
 import (
